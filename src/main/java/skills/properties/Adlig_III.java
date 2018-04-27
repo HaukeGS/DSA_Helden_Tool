76f25defd6bf28693d5cca0c8908160c003224d7
Jonas Moerschbach <jmoerschbach@gmail.com>
2018-04-27 21:44:35 +0200

    fixed typo in constant

--- src/main/java/skills/properties/Adlig_III.java
+++ src/main/java/skills/properties/Adlig_III.java
@@ -7,7 +7,7 @@ import aventurian.Aventurian;
 public class Adlig_III extends Property {
 
 	private static final String DESCRIPTION = "Der Charakter ist adliger Abstammung und hat Anspruch auf einen entsprechenden Titel. In manchen Bereichen unterliegt er nicht der gewöhnlichen Gerichtsbarkeit. Generell wird man ihm mehr Respekt entgegeben bringen und ihm mehr Gehör schenken. \nAllerdings hat er keine Aussicht darauf das Lehen seiner Eltern zu erben, da er ältere Geschwister besitzt oder aus einem Anderen Grund keinen Anspruch auf das Lehen hat. \n Er ist Kind eines Edelmannes oder einer Edelfrau, also eines Ritters oder eiens Junkers. \n Kann nicht mit 'Adlig II' oder 'Adlig II' gewählt werden!";
-	static final String NAME = "Adlig I";
+	static final String NAME = "Adlig III";
 	static final Predicate<Aventurian> REQ = (Aventurian a) -> !a.hasSkill(Adlig_I.NAME)
 			&& !a.hasSkill(Adlig_II.NAME);
 
