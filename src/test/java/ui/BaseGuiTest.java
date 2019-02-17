dd115edf8e2840d7f18011e677f97781b7c250e3
Jonas Mörschbach <jmoerschbach@gmail.com>
2019-02-17 21:36:25 +0100

    adapted everything to Java 11. Because I can :) Had some problems with maven headless test profile. Excited if travis works? :D

--- src/test/java/ui/BaseGuiTest.java
+++ src/test/java/ui/BaseGuiTest.java
@@ -93,7 +93,7 @@ public abstract class BaseGuiTest extends ApplicationTest {
 
 
 	public <T extends Node> T find(final String query) {
-		lookup(NodeMatchers.hasText(query));
+		//lookup(NodeMatchers.hasText(query));
 		// TestFX provides many operations to retrieve elements from the loaded GUI.
 		return lookup(query).query();
 	}
